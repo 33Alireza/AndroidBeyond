db64532cd6053e4d562de363a5750d1325b70ef7
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-03 22:30:24 -0800

    Implement preview example

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -2,8 +2,15 @@ package com.example.androidbeyond.view
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     TODO()
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
