6782832372491c4071e4a6464b551d5f89907fbb
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-03 22:33:58 -0800

    Implement text example

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,12 +1,19 @@
 package com.example.androidbeyond.view
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    TODO()
+    Text(
+        text = "Android",
+        color = Color.Cyan,
+        fontWeight = FontWeight.ExtraBold
+    )
 }
 
 @Preview
