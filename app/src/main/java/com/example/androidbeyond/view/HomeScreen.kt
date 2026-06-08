d0c92d04c5edf07b068356346f83492af25ca7b2
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-07 17:58:04 -0800

    Implement text field example

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -3,15 +3,21 @@ package com.example.androidbeyond.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -21,46 +27,78 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    var number by rememberSaveable { mutableIntStateOf(0) }
+    var firstInput by rememberSaveable { mutableStateOf("") }
+    var secondInput by rememberSaveable { mutableStateOf("") }
+    var result by rememberSaveable { mutableIntStateOf(0) }
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.Black),
+            .padding(20.dp)
+            .background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        StateLessCounterComponent(
-            number = number, onClick = {
-                number += 1
-            })
-    }
-}
-
-@Composable
-fun StateLessCounterComponent(
-    number: Int, onClick: () -> Unit
-) {
-    Text(
-        text = number.toString(), color = Color.White, fontWeight = FontWeight.Black
-    )
-    Button(
-        modifier = Modifier
-            .clip(RoundedCornerShape(15.dp))
-            .width(110.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFFFF5722),
-            contentColor = Color.Black,
-        ),
-        onClick = onClick
-    ) {
         Text(
-            text = "Click",
-            fontWeight = FontWeight.ExtraBold,
+            text = result.toString(), fontWeight = FontWeight.ExtraBold, fontSize = 20.sp
         )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextField(
+                modifier = Modifier
+                    .width(150.dp)
+                    .clip(RoundedCornerShape(30.dp)),
+                value = firstInput,
+                onValueChange = { firstInput = it },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color(0xFF2196F3),
+                    unfocusedContainerColor = Color(0xFF2196F3),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
+            )
+            TextField(
+                modifier = Modifier
+                    .width(150.dp)
+                    .clip(RoundedCornerShape(30.dp)),
+                value = secondInput,
+                onValueChange = { secondInput = it },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color(0xFF2196F3),
+                    unfocusedContainerColor = Color(0xFF2196F3),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(
+                modifier = Modifier, onClick = {
+                    result = firstInput.toInt() + secondInput.toInt()
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFE91E63)
+                )
+            ) {
+                Text("Calculate")
+            }
+        }
     }
 }
 
