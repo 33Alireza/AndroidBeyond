27fd2679d1c58a91e23f3b660cc35c919e8ccc5e
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-07 18:20:59 -0800

    Implement lazy column example

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,28 +1,19 @@
 package com.example.androidbeyond.view
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,72 +22,67 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    var firstInput by rememberSaveable { mutableStateOf("") }
-    var secondInput by rememberSaveable { mutableStateOf("") }
-    var result by rememberSaveable { mutableIntStateOf(0) }
-
+    val moviesList = mutableListOf(
+        "Interstellar",
+        "TENET",
+        "oppenheimer",
+        "The Odyssey",
+        "Pulp Fiction",
+        "Django Unchained",
+        "Inglourious Basterds",
+        "The Hateful Eight",
+        "Casino",
+        "Goodfellas",
+        "The Irishman",
+        "killers of the Flower Moon",
+        "Arrival",
+        "Prisoners",
+        "Dune",
+        "The Last Duel",
+        "House of Gucci",
+        "The Martian",
+        "Napoleon",
+        "Snatch",
+        "Rush",
+        "Another Round"
+    )
+    val colorsList = mutableListOf(
+        Color(0xFFE91E63),
+        Color(0xFFF44336),
+        Color(0xFF9C27B0),
+        Color(0xFF673AB7),
+        Color(0xFF3F51B5),
+        Color(0xFF2196F3),
+        Color(0xFF03A9F4),
+        Color(0xFF00BCD4),
+        Color(0xFF009688),
+        Color(0xFF4CAF50),
+        Color(0xFF8BC34A),
+        Color(0xFFCDDC39),
+        Color(0xFFFFEB3B),
+        Color(0xFFFFC107),
+        Color(0xFFFF9800),
+        Color(0xFFFF5722),
+    )
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(20.dp)
-            .background(Color.White),
+            .padding(20.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.Start
     ) {
-        Text(
-            text = result.toString(), fontWeight = FontWeight.ExtraBold, fontSize = 20.sp
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            TextField(
-                modifier = Modifier
-                    .width(150.dp)
-                    .clip(RoundedCornerShape(30.dp)),
-                value = firstInput,
-                onValueChange = { firstInput = it },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color(0xFF2196F3),
-                    unfocusedContainerColor = Color(0xFF2196F3),
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent
-                )
-            )
-            TextField(
-                modifier = Modifier
-                    .width(150.dp)
-                    .clip(RoundedCornerShape(30.dp)),
-                value = secondInput,
-                onValueChange = { secondInput = it },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color(0xFF2196F3),
-                    unfocusedContainerColor = Color(0xFF2196F3),
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent
-                )
-            )
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(
-                modifier = Modifier, onClick = {
-                    result = firstInput.toInt() + secondInput.toInt()
-                }, colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFFE91E63)
-                )
-            ) {
-                Text("Calculate")
+        LazyColumn {
+            items(moviesList) { movie ->
+                Card(
+                    modifier = Modifier, colors = CardDefaults.cardColors(
+                        containerColor = colorsList.random()
+                    )
+                ) {
+                    Text(
+                        text = movie, fontWeight = FontWeight.ExtraBold, fontSize = 30.sp
+                    )
+                }
+                Spacer(Modifier.height(20.dp))
             }
         }
     }
