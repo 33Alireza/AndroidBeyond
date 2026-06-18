bcca139fc8b0452e4a5e7da36fd94c19cf785fa7
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-17 22:46:11 -0800

    Implement content uri example

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.androidbeyond
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
 import com.example.androidbeyond.view.HomeScreen
@@ -26,10 +28,16 @@ class MainActivity : ComponentActivity() {
         FileOutputStream(file).use {
             it.write(launcherBytes)
         }
-
         println(file.toUri())
+
         setContent {
             AndroidBeyondTheme {
+                val pickImage = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.GetContent(),
+                    onResult = { contentUri ->
+                        println(contentUri)
+                    }
+                )
                 HomeScreen()
             }
         }
