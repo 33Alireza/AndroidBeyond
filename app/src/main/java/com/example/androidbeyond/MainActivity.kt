b67fdd153f9d94371b219988d761189feeea9074
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-19 22:39:25 -0800

    Implement version qualifiers example

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
-import com.example.androidbeyond.view.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +14,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBeyondTheme {
-                Image(
-                    painter = painterResource(R.drawable.ic_launcher_background),
-                    contentDescription = null
-                )
             }
         }
     }
