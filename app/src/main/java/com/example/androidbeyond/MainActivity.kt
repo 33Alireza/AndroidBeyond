c8176a0b1b486e02037c4910bafdbd6b9a94ff31
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-19 22:34:48 -0800

    Implement drawable resources use case example

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
 import com.example.androidbeyond.view.HomeScreen
@@ -11,12 +13,14 @@ import com.example.androidbeyond.view.HomeScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        resources.getDrawable(R.drawable.ic_launcher_foreground)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AndroidBeyondTheme {
-                HomeScreen()
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null
+                )
             }
         }
     }
