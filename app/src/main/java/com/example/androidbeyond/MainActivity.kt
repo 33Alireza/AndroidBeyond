4716d4a4895d1bb615feca11d19b0883237e39ca
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-26 23:31:41 -0800

    Implement home view model example

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.androidbeyond
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
+import com.example.androidbeyond.view.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,37 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBeyondTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(
-                        onClick = {
-                            /*Intent(applicationContext, SecondActivity::class.java).also {
-                                startActivity(it)
-                            }*/
-                            /*Intent(Intent.ACTION_MAIN).also {
-                                it.`package` = "com.google.android.youtube"
-                                try {
-                                    startActivity(it)
-                                } catch (e: ActivityNotFoundException) {
-                                    e.printStackTrace()
-                                }
-                            }*/
-                            val intent = Intent(Intent.ACTION_SEND).apply {
-                                type = "text/plain"
-                                putExtra(Intent.EXTRA_EMAIL, arrayOf("test@test.com"))
-                                putExtra(Intent.EXTRA_SUBJECT, arrayOf("Subject"))
-                                putExtra(Intent.EXTRA_TEXT, arrayOf("Yo!"))
-                            }
-                            if (intent.resolveActivity(packageManager) != null) {
-                                startActivity(intent)
-                            }
-                        }) {
-                        Text("Click Me")
-                    }
-                }
+                HomeScreen()
             }
         }
     }
