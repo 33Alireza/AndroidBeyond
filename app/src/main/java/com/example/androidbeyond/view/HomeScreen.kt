a6d84e40e6b161787a998b9ce993cc4f1764e2d7
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-19 21:04:31 +0330

    Counter example using compose

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,13 +1,16 @@
 package com.example.androidbeyond.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androidbeyond.viewmodel.HomeViewModel
 
@@ -16,15 +19,22 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     homeViewModel: HomeViewModel = viewModel(),
 ) {
-    val flowBackgroundColor = homeViewModel.flowBackgroundColor.collectAsStateWithLifecycle().value
-    val composeBackgroundColor = homeViewModel.composeBackgroundColor
+    val counter = homeViewModel.counterValue
 
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
-            .background(flowBackgroundColor)
-            .clickable(onClick = { homeViewModel.changeColor() })
-    ) { }
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(counter.toString())
+        Button(
+            onClick = { homeViewModel.initialCounter() }
+        ) {
+            Text("Yo!")
+        }
+    }
 }
 
 @Preview
--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -3,27 +3,16 @@ package com.example.androidbeyond.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import java.math.BigInteger
 
 class HomeViewModel : ViewModel() {
-    private var _flowBackgroundColor = MutableStateFlow(Color.White)
-    val flowBackgroundColor = _flowBackgroundColor.asStateFlow()
-
-    var composeBackgroundColor by mutableStateOf(Color.White)
+    var counterValue: BigInteger by mutableStateOf(BigInteger.ONE)
         private set
 
-    private val colorsList = listOf(
-        Color(0xFFE91E63),
-        Color(0xFF2196F3),
-        Color(0xFFFF9800),
-        Color(0xFF4CAF50),
-    )
-
-    fun changeColor() {
-        _flowBackgroundColor.value = colorsList.random()
-        composeBackgroundColor = colorsList.random()
+    fun initialCounter() {
+        while (true) {
+            counterValue += counterValue
+        }
     }
 }
