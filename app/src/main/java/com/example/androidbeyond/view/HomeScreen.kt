1e5528e67a0c7f0f5ce95e9ba0ec38a417532d85
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-19 21:51:02 +0330

    Correct implementation of encapsulation in state flow

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,50 +1,51 @@
 package com.example.androidbeyond.view
 
-import androidx.compose.foundation.background
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androidbeyond.viewmodel.HomeViewModel
-import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.milliseconds
+import kotlinx.coroutines.flow.MutableStateFlow
 
+@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     homeViewModel: HomeViewModel = viewModel(),
 ) {
-    val color = homeViewModel.backgroundColor.collectAsStateWithLifecycle().value
-
-    SideEffect {
-        // non-suspend method
-        homeViewModel.changeColor()
-    }
-
-    LaunchedEffect(Unit) {
-        delay(5000L.milliseconds)
-        homeViewModel.changeColor()
-    }
-
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(color)
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Yo!")
+        Button(
+            onClick = {
+                try {
+                    (homeViewModel.numberOne as MutableStateFlow).value = 6
+                    println("number one casted successfully")
+                } catch (_: Exception) {
+                    println("number one was not able to cast")
+                }
+                try {
+                    (homeViewModel.numberTwo as MutableStateFlow).value = 8
+                    println("number two casted successfully")
+                } catch (_: Exception) {
+                    println("number two was not able to cast")
+                }
+            }) {
+            Text("Yo!")
+        }
     }
 }
 
--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,23 +1,15 @@
 package com.example.androidbeyond.viewmodel
 
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class HomeViewModel : ViewModel() {
+    // correct version
+    private val _numberOne = MutableStateFlow(3)
+    val numberOne = _numberOne.asStateFlow()
 
-    private var _backgroundColor = MutableStateFlow(Color.White)
-    val backgroundColor = _backgroundColor.asStateFlow()
-
-    private val colorsList = listOf(
-        Color(0xFFE91E63),
-        Color(0xFF2196F3),
-        Color(0xFFFF9800),
-        Color(0xFF4CAF50),
-    )
-
-    fun changeColor() {
-        _backgroundColor.value = colorsList.random()
-    }
+    // wrong version
+    var numberTwo = MutableStateFlow(4)
+        private set
 }
