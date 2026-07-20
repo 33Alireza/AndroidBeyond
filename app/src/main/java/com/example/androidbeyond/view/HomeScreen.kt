2a576ac7f86713f2430958480ebfd0034e7213cb
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-20 18:50:39 +0330

    Retain Api Example

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -8,29 +8,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.retain.retain
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.androidbeyond.viewmodel.HomeViewModel
 
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    homeViewModel: HomeViewModel = viewModel(),
 ) {
-    val number = homeViewModel.number.collectAsStateWithLifecycle()
+    var number by retain { mutableIntStateOf(0) }
 
-    DisposableEffect(number) {
-        println("Composition happened!")
-        onDispose {
-            println("Decomposition happened!")
-        }
-    }
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -38,8 +31,11 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(number.toString())
         Button(
-            onClick = { homeViewModel.increaseNumber() }) {
+            onClick = {
+                number += 1
+            }) {
             Text("Yo!")
         }
     }
