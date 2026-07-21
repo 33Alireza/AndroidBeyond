2eee62f093a4a3e2cce5a5e48decf982b615674d
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-21 20:23:25 +0330

    Delegation -> AnalyticsLogger

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -5,18 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
 import com.example.androidbeyond.view.HomeScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), AnalyticsLogger by AnalyticsLoggerImpl() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerLifecycleOwner(this)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AndroidBeyondTheme {
                 HomeScreen()
             }
+        }
+    }
+}
+
+interface AnalyticsLogger {
+    fun registerLifecycleOwner(owner: LifecycleOwner)
+}
+
+class AnalyticsLoggerImpl : AnalyticsLogger, LifecycleEventObserver {
+
+    override fun registerLifecycleOwner(owner: LifecycleOwner) {
+        owner.lifecycle.addObserver(this)
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_START -> println("User opened the screen")
+            Lifecycle.Event.ON_PAUSE -> println("User leaved the screen")
+            else -> Unit
         }
     }
 }
