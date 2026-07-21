de8fa9378ff9407dce5c12d338a8677ee459e348
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-21 19:57:59 +0330

    Simple animation using infiniteTransition

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,10 +1,12 @@
 package com.example.androidbeyond.view
 
 import android.annotation.SuppressLint
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -29,18 +31,25 @@ fun HomeScreen(
     var sizeState by remember { mutableStateOf(200.dp) }
     val size by animateDpAsState(
         targetValue = sizeState,
-        keyframes {
-            durationMillis = 5000
-            sizeState at 0 using LinearEasing
-            sizeState * 1.5f at 1000 using FastOutLinearInEasing
-            sizeState * 2f at 5000
-        }
+        tween(
+            durationMillis = 1000
+        )
+    )
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 2000),
+            repeatMode = RepeatMode.Reverse
+        )
     )
 
     Box(
         modifier = modifier
             .size(size)
-            .background(Color.Red),
+            .background(color),
         contentAlignment = Alignment.Center
     ) {
         Button(
