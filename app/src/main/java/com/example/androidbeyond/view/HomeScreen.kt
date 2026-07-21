f70fd031b953035b4c815a40936677a5fbdc1e19
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-21 19:40:13 +0330

    Simple animation using state

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,20 +1,19 @@
 package com.example.androidbeyond.view
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.retain.RetainedEffect
-import androidx.compose.runtime.retain.retain
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -23,26 +22,20 @@ import androidx.compose.ui.unit.dp
 fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
-    var number by retain { mutableIntStateOf(0) }
+    var sizeState by remember { mutableStateOf(200.dp) }
 
-    RetainedEffect(number) {
-        onRetire {
-            println("Retained happened!")
-        }
-    }
-    Column(
+    Box(
         modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .size(sizeState)
+            .background(Color.Red),
+        contentAlignment = Alignment.Center
     ) {
-        Text(number.toString())
         Button(
             onClick = {
-                number += 1
-            }) {
-            Text("Yo!")
+                sizeState += 50.dp
+            }
+        ) {
+            Text("Increase Size")
         }
     }
 }
