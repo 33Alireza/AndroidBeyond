beaeddb2aa826ee65819e37e3248dbeab90a88b9
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-22 20:32:50 +0330

    Edge to Edge -> Window Insets

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.androidbeyond.view
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -9,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
@@ -17,15 +21,18 @@ fun HomeScreen(
 ) {
     Scaffold(
         modifier = modifier,
+        contentWindowInsets = WindowInsets.safeDrawing
     ) { innerPadding ->
         LazyColumn(
-            modifier = Modifier
-                .padding(innerPadding)
+            contentPadding = innerPadding,
         ) {
             items(100) {
                 Text(
                     text = "Android",
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
                 )
             }
         }
