5a78c022dca654f87699b29d93b239bc8622b3b3
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-22 20:28:04 +0330

    Edge to Edge -> Inner Padding

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -7,14 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
 import com.example.androidbeyond.view.HomeScreen
-import kotlin.reflect.KProperty
 
 class MainActivity : ComponentActivity() {
-    private val obj by MyLazy {
-        println("Hello, World!")
-        3
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
@@ -24,19 +18,5 @@ class MainActivity : ComponentActivity() {
                 HomeScreen()
             }
         }
-        println(obj)
-    }
-}
-
-class MyLazy<out T : Any>(
-    private var initialize: () -> T
-) {
-    private var value: T? = null
-
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        return if (value == null) {
-            value = initialize()
-            value!!
-        } else value!!
     }
 }
--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -1,63 +1,33 @@
 package com.example.androidbeyond.view
 
 import android.annotation.SuppressLint
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
-    var sizeState by remember { mutableStateOf(200.dp) }
-    val size by animateDpAsState(
-        targetValue = sizeState,
-        tween(
-            durationMillis = 1000
-        )
-    )
-
-    val infiniteTransition = rememberInfiniteTransition()
-    val color by infiniteTransition.animateColor(
-        initialValue = Color.Red,
-        targetValue = Color.Green,
-        animationSpec = infiniteRepeatable(
-            tween(durationMillis = 2000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-
-    Box(
-        modifier = modifier
-            .size(size)
-            .background(color),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(
-            onClick = {
-                sizeState += 50.dp
-            }
+    Scaffold(
+        modifier = modifier,
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
         ) {
-            Text("Increase Size")
+            items(100) {
+                Text(
+                    text = "Android",
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
