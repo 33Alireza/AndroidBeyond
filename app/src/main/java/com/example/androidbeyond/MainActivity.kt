029672688eed10005e7408a3da54f0cc2e3a1c96
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-26 19:57:34 +0330

    Coroutine practice -> GlobalScope Test

--- app/src/main/java/com/example/androidbeyond/MainActivity.kt
+++ app/src/main/java/com/example/androidbeyond/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.androidbeyond.ui.theme.AndroidBeyondTheme
 import com.example.androidbeyond.view.HomeScreen
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 class MainActivity : ComponentActivity() {
     @OptIn(DelicateCoroutinesApi::class)
@@ -18,6 +22,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidBeyondTheme {
                 HomeScreen()
+            }
+        }
+        var number = 1
+        GlobalScope.launch {
+            while (true) {
+                delay(1000.milliseconds)
+                println(number)
+                number++
             }
         }
     }
