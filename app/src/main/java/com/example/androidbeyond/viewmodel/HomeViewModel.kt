74d3ae3ac9e003bdcd8bb29789569d4e6db8ec83
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-27 19:11:44 +0330

    Coroutine practice -> ConcurrentUserProfile -> fetchBasicInfo method

--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,10 +1,22 @@
 package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import kotlin.time.Duration.Companion.milliseconds
 
 class HomeViewModel : ViewModel() {
-
+    private suspend fun fetchBasicInfo(): BasicInfo = withContext(Dispatchers.IO) {
+        delay(1200.milliseconds)
+        BasicInfo("Alireza", "alireza@gmail.com")
+    }
 }
+
+data class BasicInfo(
+    val name: String,
+    val email: String
+)
 
 data class UserProfile(
     val name: String,
