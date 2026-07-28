c31660a1c4a844ab02167ccca2c1f611d57408c6
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-28 20:37:25 +0330

    Coroutine practice -> ConcurrentProductInfo -> loadProductDetails method

--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -18,10 +24,40 @@ class HomeViewModel : ViewModel() {
     private val _isLoading = MutableStateFlow(false)
     val isLoading = _isLoading.asStateFlow()
 
-    private val _event = MutableSharedFlow<String>()
+    private val _event = MutableSharedFlow<String>(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_LATEST
+    )
     val event = _event.asSharedFlow()
 
     private val job: Job? = null
+
+    fun loadProductDetails() {
+        job?.cancel()
+        _product.value = null
+        viewModelScope.launch {
+            _isLoading.value = true
+            try {
+                coroutineScope {
+                    val deferredProductInfo = async { fetchProductInfo() }
+                    val deferredReviews = async { fetchReviews() }
+                    val deferredRelatedProducts = async { fetchRelatedProducts() }
+
+                    val productInfo = deferredProductInfo.await()
+                    val reviews = deferredReviews.await()
+                    val relatedProducts = deferredRelatedProducts.await()
+
+                    _product.value = ProductDetails(productInfo, reviews, relatedProducts)
+                }
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                _event.tryEmit(e.message ?: "Unknown Error")
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
 
     private suspend fun fetchProductInfo(): ProductInfo = withContext(Dispatchers.IO) {
         delay(1000.milliseconds)
