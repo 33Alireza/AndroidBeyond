f25adcae7b378a21d624d9a961ffb3fea4e9cd1d
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-28 20:20:20 +0330

    Coroutine practice -> ConcurrentProductInfo -> StateFlows & SharedFlow

--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class HomeViewModel : ViewModel() {
+    private val _product = MutableStateFlow<ProductDetails?>(null)
+    val product = _product.asStateFlow()
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _event = MutableSharedFlow<String>()
+    val event = _event.asSharedFlow()
+
+    private val job: Job? = null
 }
 
 data class ProductDetails(
