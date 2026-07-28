15e49bf599b52da83bffea0acc187979d0d565ae
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-28 20:24:45 +0330

    Coroutine practice -> ConcurrentProductInfo -> fetchProductInfo method

--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.withContext
+import kotlin.time.Duration.Companion.milliseconds
 
 class HomeViewModel : ViewModel() {
     private val _product = MutableStateFlow<ProductDetails?>(null)
@@ -18,6 +22,15 @@ class HomeViewModel : ViewModel() {
     val event = _event.asSharedFlow()
 
     private val job: Job? = null
+
+    private suspend fun fetchProductInfo(): ProductInfo = withContext(Dispatchers.IO) {
+        delay(1000.milliseconds)
+        if ((1..100).random() > 80) {
+            throw Exception("Yo!")
+        } else {
+            ProductInfo("MacBook Pro M5", 2.000, "16GB, 512GB, 14Inch")
+        }
+    }
 }
 
 data class ProductDetails(
