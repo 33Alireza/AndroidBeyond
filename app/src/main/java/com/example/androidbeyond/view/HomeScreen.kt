de42db8d8cf9cff9eb8eff60e4e4ab5dcab3254c
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-04 09:33:54 +0330

    Flow -> countdown

--- app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
+++ app/src/main/java/com/example/androidbeyond/view/HomeScreen.kt
@@ -4,19 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androidbeyond.viewmodel.HomeViewModel
@@ -25,46 +18,18 @@ import com.example.androidbeyond.viewmodel.HomeViewModel
 fun HomeScreen(
     modifier: Modifier = Modifier, viewModel: HomeViewModel = viewModel()
 ) {
-    val product = viewModel.product.collectAsStateWithLifecycle()
-    val isLoading = viewModel.isLoading.collectAsStateWithLifecycle()
-    val event = viewModel.event
-    val snackBarHostState = remember { SnackbarHostState() }
-    val actionLabel = "Retry"
+    val time = viewModel.countDownFlow.collectAsStateWithLifecycle(10)
 
-    LaunchedEffect(Unit) { viewModel.loadProductDetails() }
-    LaunchedEffect(Unit) {
-        event.collect {
-            val result = snackBarHostState.showSnackbar(
-                message = it, actionLabel = actionLabel, duration = SnackbarDuration.Long
-            )
-            if (result == SnackbarResult.ActionPerformed) {
-                viewModel.loadProductDetails()
-            }
-        }
-    }
-
-    Scaffold(
-        modifier = modifier,
-        snackbarHost = { SnackbarHost(snackBarHostState) }) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            if (isLoading.value) {
-                CircularProgressIndicator()
-            } else {
-                product.value?.let {
-                    Text(it.info.name)
-                    Text(it.info.price.toString())
-                    Text(it.info.description)
-                    Text(it.reviews.joinToString(", "))
-                    Text(it.relatedProducts.joinToString(", "))
-                }
-            }
-        }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = time.value.toString()
+        )
     }
 }
 
--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,100 +1,19 @@
 package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.flow
 import kotlin.time.Duration.Companion.milliseconds
 
 class HomeViewModel : ViewModel() {
-    private val _product = MutableStateFlow<ProductDetails?>(null)
-    val product = _product.asStateFlow()
-
-    private val _isLoading = MutableStateFlow(false)
-    val isLoading = _isLoading.asStateFlow()
-
-    private val _event = MutableSharedFlow<String>(
-        extraBufferCapacity = 1,
-        onBufferOverflow = BufferOverflow.DROP_LATEST
-    )
-    val event = _event.asSharedFlow()
-
-    private val job: Job? = null
-
-    fun loadProductDetails() {
-        job?.cancel()
-        _product.value = null
-        viewModelScope.launch {
-            _isLoading.value = true
-            try {
-                coroutineScope {
-                    val deferredProductInfo = async { fetchProductInfo() }
-                    val deferredReviews = async { fetchReviews() }
-                    val deferredRelatedProducts = async { fetchRelatedProducts() }
-
-                    val productInfo = deferredProductInfo.await()
-                    val reviews = deferredReviews.await()
-                    val relatedProducts = deferredRelatedProducts.await()
-
-                    _product.value = ProductDetails(productInfo, reviews, relatedProducts)
-                }
-            } catch (e: CancellationException) {
-                throw e
-            } catch (e: Exception) {
-                _event.tryEmit(e.message ?: "Unknown Error")
-            } finally {
-                _isLoading.value = false
-            }
-        }
-    }
-
-    private suspend fun fetchProductInfo(): ProductInfo = withContext(Dispatchers.IO) {
-        delay(1000.milliseconds)
-        if ((1..100).random() > 80) {
-            throw Exception("Yo!")
-        } else {
-            ProductInfo("MacBook Pro M5", 2.000, "16GB, 512GB, 14Inch")
-        }
-    }
-
-    private suspend fun fetchReviews(): List<String> = withContext(Dispatchers.IO) {
-        delay(15000.milliseconds)
-        if ((1..100).random() > 80) {
-            throw Exception("Yo!")
-        } else {
-            listOf("Best Laptop ever", "Apple's finest")
-        }
-    }
-
-    private suspend fun fetchRelatedProducts(): List<String> = withContext(Dispatchers.IO) {
-        delay(1200.milliseconds)
-        if ((1..100).random() > 80) {
-            throw Exception("Yo!")
-        } else {
-            listOf("MacBook Air M5", "MAcBook Neo")
+    val countDownFlow = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            delay(1000L.milliseconds)
+            currentValue--
+            emit(currentValue)
         }
     }
 }
-
-data class ProductDetails(
-    val info: ProductInfo,
-    val reviews: List<String>,
-    val relatedProducts: List<String>
-)
-
-data class ProductInfo(
-    val name: String,
-    val price: Double,
-    val description: String
-)
