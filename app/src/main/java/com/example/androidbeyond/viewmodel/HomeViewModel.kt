b8da19856598875fd52342a34ac111d703346b31
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-06 22:14:15 +0330

    Flow practice -> ColdFlowCounter -> createNumberFlow method

--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -2,20 +2,23 @@ package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlin.time.Duration.Companion.milliseconds
 
 class HomeViewModel : ViewModel() {
-    val numberFlow = flow {
-        delay(1000.milliseconds)
-        emit(1)
-        delay(1000.milliseconds)
-        emit(2)
-        delay(1000.milliseconds)
-        emit(3)
-        delay(1000.milliseconds)
-        emit(4)
-        delay(1000.milliseconds)
-        emit(5)
+    private fun createNumberFlow(): Flow<Int> {
+        return flow {
+            delay(1000.milliseconds)
+            emit(1)
+            delay(1000.milliseconds)
+            emit(2)
+            delay(1000.milliseconds)
+            emit(3)
+            delay(1000.milliseconds)
+            emit(4)
+            delay(1000.milliseconds)
+            emit(5)
+        }
     }
 }
