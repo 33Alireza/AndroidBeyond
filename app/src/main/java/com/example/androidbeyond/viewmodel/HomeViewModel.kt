640634e218131899f2a3fc6b59f27c1a72e21a85
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-06 22:25:02 +0330

    Flow practice -> ColdFlowCounter -> startCounting method

--- app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbeyond/viewmodel/HomeViewModel.kt
@@ -1,12 +1,33 @@
 package com.example.androidbeyond.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
 
 class HomeViewModel : ViewModel() {
+    private val _currentNumber = MutableStateFlow<Int?>(null)
+    val currentNumber = _currentNumber.asStateFlow()
+
+    private val _isRunning = MutableStateFlow(false)
+    val isRunning = _isRunning.asStateFlow()
+
+    var job: Job? = null
+
+    fun startCounting() {
+        job = viewModelScope.launch {
+            createNumberFlow().collect { number ->
+                _currentNumber.value = number
+            }
+        }
+    }
+
     private fun createNumberFlow(): Flow<Int> {
         return flow {
             delay(1000.milliseconds)
